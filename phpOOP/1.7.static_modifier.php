2f90f3593caff915752cab3689fbb4ae86b0d75d
marko2107 <marko.drnasin@gmail.com>
2018-12-11 13:27:18 +0100

    static modifier

--- phpOOP/1.7.static_modifier.php
+++ phpOOP/1.7.static_modifier.php
@@ -1,23 +1,16 @@
 <?php
 
 class Cars {
-    public $wheel_count=4;
-    private $door_count=4;
-    protected $seat_count=2;
-
-    function car_detail() {
-        echo $this->wheel_count;
-        echo $this->door_count;
-        echo $this->seat_count;
+    static $wheel_count=4;
+    static $door_count=4;
+    
+    static function car_detail() { //kad je static ne treba nam $this
+        echo Cars::wheel_count;
+        echo Cars::door_count;
     }
 
 }
 
-$bmw = new Cars();
 
-//echo $bmw->wheel_count; ispiše ga
-//echo $bmw->door_count; ne ispiše ga (privatan)
-//echo $bmw->seat_count; ne ispiše ga (zaštičen)
-
-$bmw->car_detail();
-
+echo Cars::$wheel_count;
+echo Cars::$door_count;
