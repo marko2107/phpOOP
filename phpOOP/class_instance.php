bc07c8543f9e2cdb28d93ed49141897d254fe0f5
marko2107 <marko.drnasin@gmail.com>
2018-12-11 12:46:59 +0100

    class propreties

---
+++ phpOOP/class_instance.php
@@ -0,0 +1,14 @@
+<?php
+
+class Cars {
+
+    function gretting() {
+        echo "Hello Student";
+    }
+
+}
+
+$bmw = new Cars();
+$mercedes = new Cars();
+
+$bmw->gretting();
---
+++ phpOOP/class_properties.php
@@ -0,0 +1,21 @@
+<?php
+
+class Cars {
+    var $wheel_count=4;
+    var $door_count=4;
+
+    function car_detail() {
+        return "This car has ".$this->wheel_count." wheels";
+    }
+
+}
+
+$bmw = new Cars();
+$mercedes = new Cars();
+
+echo $bmw->wheel_count=10 .'<br>';
+echo $mercedes->wheel_count;
+echo '<br>';
+echo $mercedes->car_detail();
+echo '<br>';
+echo $bmw->car_detail();
