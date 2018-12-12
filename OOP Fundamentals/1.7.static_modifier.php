a5a355de82c496248ec15145c4f780c8849ca321
marko2107 <marko.drnasin@gmail.com>
2018-12-12 11:02:23 +0100

    break down admin nav

--- OOP Fundamentals/1.7.static_modifier.php
+++ OOP Fundamentals/1.7.static_modifier.php
@@ -3,7 +3,7 @@
 class Cars {
     static $wheel_count=4;
     static $door_count=4;
-    
+
     static function car_detail() { //kad je static ne treba nam $this
         echo Cars::wheel_count;
         echo Cars::door_count;
